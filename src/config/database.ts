526bf1b55e971c79c55c7e7af90fe00e6e882d93
Lucas Glave <lucasglave7@gmail.com>
2024-10-06 20:48:12 -0300

    deploy in render

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,16 +3,34 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASS as string,
-    {
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        dialect: 'postgres',
-        logging: false
+const isProd = process.env.NODE_ENV === "prod";
+
+let sequelize: Sequelize;
+
+if (isProd) {
+    const connectionProd = process.env.DB_CONNECTION_INT;
+    if (!connectionProd) {
+        throw new Error("La cadena de conexión a la base de datos no está definida en producción.");
     }
-);
+    sequelize = new Sequelize(connectionProd, {
+        dialect: "postgres",
+        logging: false,
+    });
+} else {
+    const connectionDev = {
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        host: process.env.DB_HOST,
+    };
+    if (!connectionDev.database || !connectionDev.username || !connectionDev.password || !connectionDev.host) {
+        throw new Error("Faltan configuraciones para la conexión a la base de datos en desarrollo.");
+    }
+    sequelize = new Sequelize(connectionDev.database, connectionDev.username, connectionDev.password, {
+        host: connectionDev.host,
+        dialect: "postgres",
+        logging: false,
+    });
+}
 
 export default sequelize;
